2be83c5d7ec93de761658292830f81fc094a174a
Henrik Joreteg <hjoreteg@gmail.com>
2013-09-03 20:45:42 -0700

    making build process work again (less dev)

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ var async = require('async'),
 	fs = require('fs'),
 	ncp = require('ncp').ncp,
 	rimraf = require('rimraf'),
-    stitchPackage = require('./stitchPackage'),
+    clientPackage = require('./clientPackage'),
 	UglifyJS = require('uglify-js');
 
 var version;
@@ -16,56 +16,42 @@ async.series([
 	},
 	function (cb) {
 		console.log('Making build folders...'.green);
-		fs.mkdir('build', cb);
+		fs.mkdirSync('build');
+		//fs.mkdirSync('build/dev');
+		fs.mkdirSync('build/prod');
+		cb();
 	},
-	function (cb) {
-		fs.mkdir('build/dev', cb);
-	},
-	function (cb) {
-		fs.mkdir('build/prod', cb);
-	},
+	/*
 	function (cb) {
 		console.log('Moving images to DEV...'.green);
 		ncp('public/img', 'build/dev/img', cb);
 	},
-	function (cb) {
-		console.log('Moving CSS to DEV...'.green);
-		ncp('public/css', 'build/dev/css', cb);
-	},
+	*/
 	function (cb) {
 		console.log('Moving images to PROD...'.green);
 		ncp('public/img', 'build/prod/img', cb);
 	},
 	function (cb) {
-		console.log('Moving CSS to PROD...'.green);
-		ncp('public/css', 'build/prod/css', cb);
-	},
-	function (cb) {
 		console.log('Creating packages...'.magenta);
-		stitchPackage.compile(function (err, source) {
-			var shasum = crypto.createHash('sha1');
-			shasum.update(source);
-			version = shasum.digest('hex').slice(0, 8);
+		var clientApp = clientPackage(false);
+		clientApp._ensureReady(function (err, source) {
+			var result = clientApp.result;
 
-			console.log('Writing DEV version %s to file...'.green, version);
-		    fs.writeFile('build/dev/app.' + version + '.js', source, cb);
+			console.log('Writing minified CSS to PROD folder...'.green);
+			//fs.writeFileSync('build/dev/' + result.cssFileName, result.css);
+			fs.writeFileSync('build/prod/' + result.cssFileName, result.css);
 
-			console.log('Writing and minifying PROD version %s to file...'.green, version);
-			var minSource = UglifyJS.minify(source, {fromString: true}).code;
-		    fs.writeFile('build/prod/app.' + version + '.js', minSource, cb);
-		});
-	},
-	function (cb) {
-		fs.readFile('index.html', 'utf-8', function (error, buffer) {
-			var source = buffer.toString();
-			console.log('Replacing version string in HTML index file to be %s...'.green, version);
-			source = source.replace(/\{\{version\}\}/g, version);
-			console.log('Moving HTML index files...'.green);
-			fs.writeFile('build/dev/index.html', source, null);
-			fs.writeFile('build/prod/index.html', source, cb);
+			console.log('Writing html template to PROD folder...'.green);
+			//fs.writeFileSync('build/dev/index.html', result.html);
+			fs.writeFileSync('build/prod/index.html', result.html);
+
+			console.log('Writing minified PROD version to file...'.green);
+			fs.writeFileSync('build/prod/' + result.jsMinFileName, result.minSource);
+
+			cb();
 		});
 	}
-], function (error) { 
+], function (error) {
 	if (error === null) {
 		console.log('No errors from build process.'.green);
 	} else {
